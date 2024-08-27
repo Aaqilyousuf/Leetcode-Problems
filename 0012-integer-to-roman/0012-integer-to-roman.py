c915b6f1f47546ed33f94482e1cd96db0095e161
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2024-08-27 15:02:02 +0530

    Time: 39 ms (90.45%), Space: 16.6 MB (55.08%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,19 +1,13 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        # Define a list of tuples with the value and corresponding Roman numeral
-        val_to_roman = [
-            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
-            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
-            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
-        ]
-        
-        roman = ""  # This will hold the final Roman numeral
-        
-        # Loop over the values and numerals
-        for value, numeral in val_to_roman:
-            # While num is larger than the value, append the numeral and subtract the value
-            while num >= value:
-                roman += numeral
-                num -= value
-        
-        return roman
+        d = {
+            'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 
+            'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 
+            'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1
+        }
+        result = ""
+        for i in d:
+            while num >= d[i]:
+                result += i
+                num -= d[i]
+        return result
