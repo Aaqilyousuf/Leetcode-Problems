003c45781c4377a40c17139834d4b0eda1949710
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2024-09-03 23:39:38 +0530

    Time: 33 ms (85.81%), Space: 16.5 MB (73.84%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        res = ""
+        ans = 0
+        for i in s:
+            val = ord(i) - ord('a') + 1
+            res += str(val) 
+        
+        while k > 0:
+            for j in res:
+                ans = ans + int(j)
+            res = str(ans)
+            if k > 1:
+                ans = 0
+            k -= 1
+        return ans
+
+        
