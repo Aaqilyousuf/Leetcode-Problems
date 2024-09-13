274600624812c2cadd035ba6a5a5cc137c0e33fc
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2024-09-13 21:23:28 +0530

    Time: 37 ms (63.6%), Space: 16.5 MB (36.22%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -5,7 +5,6 @@ class Solution:
         for i in s:
             val = ord(i) - ord('a') + 1
             res += str(val) 
-        
         while k > 0:
             for j in res:
                 ans = ans + int(j)
