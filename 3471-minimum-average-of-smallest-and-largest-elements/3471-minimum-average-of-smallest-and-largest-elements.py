f147e83302f81dff4de5e122a76a2ff6f06f79fc
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2024-09-24 09:35:17 +0530

    Time: 48 ms (51.97%), Space: 16.5 MB (86.46%) - LeetHub

--- 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.py
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.py
@@ -1,16 +1,25 @@
 class Solution:
     def minimumAverage(self, nums: List[int]) -> float:
-        avg = []
+        # avg = []
     
-        while nums:
-            large = max(nums)
-            small = min(nums)
-            a = (small+large)/2
-            avg.append(a)
-            if large == small:
-                nums.remove(large)
-            else:
-                nums.remove(large) 
-                nums.remove(small) 
+        # while nums:
+        #     large = max(nums)
+        #     small = min(nums)
+        #     a = (small+large)/2
+        #     avg.append(a)
+        #     if large == small:
+        #         nums.remove(large)
+        #     else:
+        #         nums.remove(large) 
+        #         nums.remove(small) 
 
+        # return min(avg)
+        nums.sort()
+        avg = []
+        n = len(nums)
+        for i in range(n//2):
+            minEl = nums[i]
+            maxEl = nums[n-1-i]
+            a = (minEl+maxEl)/2
+            avg.append(a)
         return min(avg)
