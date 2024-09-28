220835cc77c00b72221d3418a10370a56e7c059b
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2024-09-28 12:12:00 +0530

    Time: 208 ms (18.8%), Space: 19.1 MB (79.19%) - LeetHub

---
+++ 1582-design-browser-history/1582-design-browser-history.py
@@ -0,0 +1,28 @@
+from collections import deque 
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.st1 = [homepage]
+        self.st2 = []
+    def visit(self, url: str) -> None:
+        self.st1.append(url)
+        self.st2.clear()
+    def back(self, steps: int) -> str:
+        while steps > 0 and len(self.st1) > 1:
+            self.st2.append(self.st1.pop())
+            steps-=1
+        return self.st1[-1]
+
+    def forward(self, steps: int) -> str:
+        while steps > 0 and self.st2:
+            self.st1.append(self.st2.pop())
+            steps -= 1
+        return self.st1[-1]
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
