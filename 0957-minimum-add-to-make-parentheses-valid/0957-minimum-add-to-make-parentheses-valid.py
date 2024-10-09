b49d6ab78766cccb3102c44ca4330ceb911f497b
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2024-10-09 09:25:48 +0530

    Time: 34 ms (65.4%), Space: 16.4 MB (94.42%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        a = 0
+        res = 0
+        for i in s:
+            if i=="(":
+                a+=1
+            elif a>0:
+                a-=1
+            else:
+                res+=1
+        return res+a        
