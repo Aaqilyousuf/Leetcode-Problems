2ba7699dca9fd9bec17d15560e2605e92495bc4e
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-01-06 11:33:35 +0530

    Time: 6 ms (95.28%), Space: 74.8 MB (42.92%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.java
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.java
@@ -1,29 +1,31 @@
 class Solution {
     public String shiftingLetters(String s, int[][] shifts) {
+        //in this program we used difference array technique
         int n = s.length();
-        int[] arr = new int[n];
+        int[] arr = new int[n+1]; //increase the size by becasue for handle edge case
         for (int[] shift : shifts) {
             if (shift[2] == 1) { //fw
                 arr[shift[0]]++;
-                if (shift[1] + 1 < n) {
-                    arr[shift[1] + 1]--;
-                }
+                arr[shift[1] + 1]--;
+                
             } else { //bw
                 arr[shift[0]]--;
-                if (shift[1] + 1 < n) {
-                    arr[shift[1] + 1]++;
-                }
+                arr[shift[1] + 1]++;
             }
+        }
+        //compute prefix sum actuall shifts
+        for(int i=1;i<=n;i++){
+            arr[i] += arr[i-1];
         }
 
         StringBuilder result = new StringBuilder(s);
-        int sum = 0;
         for (int i = 0; i < n; i++) {
-            sum = (sum + arr[i]) % 26; 
-            if (sum < 0) sum += 26;
-            char shiftedChar = (char) ('a' +
-                ((s.charAt(i) - 'a' + sum) % 26));
-            result.setCharAt(i, shiftedChar);
+            int count = arr[i] % 26;
+            if(count<0){
+                count += 26;
+            }
+            char cur = (char) (((s.charAt(i)) - 'a' + count) % 26 + 'a');
+            result.setCharAt(i, cur);
         }
         return result.toString();
     }
