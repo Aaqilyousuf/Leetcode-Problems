43f0651a9eaaeb0ddfafaa95ab0e55022c825146
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-01-08 15:14:14 +0530

    Time: 24 ms (8.87%), Space: 43.1 MB (77.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,32 +1,3 @@
-// import java.util.HashMap;
-
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//         if (s.length() != t.length()) {
-//             return false;
-//         }
-
-//         HashMap<Character, Integer> map = new HashMap<>();
-
-//         // Count characters from `s` and `t`
-//         for (int i = 0; i < s.length(); i++) {
-//             char c1 = s.charAt(i);
-//             char c2 = t.charAt(i);
-
-//             map.put(c1, map.getOrDefault(c1, 0) + 1);
-//             map.put(c2, map.getOrDefault(c2, 0) - 1);
-//         }
-
-//         // Check if all counts are zero
-//         for (int count : map.values()) {
-//             if (count != 0) {
-//                 return false;
-//             }
-//         }
-
-//         return true;
-//     }
-// }
 import java.util.HashMap;
 
 class Solution {
@@ -35,21 +6,50 @@ class Solution {
             return false;
         }
 
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        // Count character frequencies for `s`
-        for (char c : s.toCharArray()) {
-            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        // Count characters from `s` and `t`
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            map.put(c1, map.getOrDefault(c1, 0) + 1);
+            map.put(c2, map.getOrDefault(c2, 0) - 1);
         }
 
-        // Count character frequencies for `t`
-        for (char c : t.toCharArray()) {
-            map2.put(c, map2.getOrDefault(c, 0) + 1);
+        // Check if all counts are zero
+        for (int count : map.values()) {
+            if (count != 0) {
+                return false;
+            }
         }
 
-        // Compare the two maps
-        return map1.equals(map2);
+        return true;
     }
 }
+// import java.util.HashMap;
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+
+//         HashMap<Character, Integer> map1 = new HashMap<>();
+//         HashMap<Character, Integer> map2 = new HashMap<>();
+
+//         // Count character frequencies for `s`
+//         for (char c : s.toCharArray()) {
+//             map1.put(c, map1.getOrDefault(c, 0) + 1);
+//         }
+
+//         // Count character frequencies for `t`
+//         for (char c : t.toCharArray()) {
+//             map2.put(c, map2.getOrDefault(c, 0) + 1);
+//         }
+
+//         // Compare the two maps
+//         return map1.equals(map2);
+//     }
+// }
 
