c92f02be2ecc0bfc76a4c2bfe28dfe08ea7cde80
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-01-17 08:28:05 +0530

    Time: 0 ms (100%), Space: 19.5 MB (44.39%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,13 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        map = {}
-        for n in nums:
-            if n in map:
-                map[n] += 1 
-            else:
-                map[n] = 1
-        for n in map:
-            if map[n] == 1:
-                return n
-            
-             
+        xor = 0
+        for i in range(len(nums)):
+            xor = xor ^ nums[i]
+        return xor
