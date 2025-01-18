b7fc03b420e29a47d8eadfbd9657d7fbc38c2fa9
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-01-18 23:03:15 +0530

    Time: 0 ms (100%), Space: 17.9 MB (22.56%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,34 +1,17 @@
-from typing import List
-
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        n = len(matrix)
-        m = len(matrix[0])
-        ans = []
-        top, bottom = 0, n - 1
-        left, right = 0, m - 1
+        res = []
+        while matrix:
+            res += matrix.pop(0)
 
-        while left <= right and top <= bottom:
-            # Traverse from left to right
-            for i in range(left, right + 1):
-                ans.append(matrix[top][i])
-            top += 1
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop())
+                
+            if matrix and matrix[0]:
+                res += matrix.pop()[::-1]
 
-            # Traverse from top to bottom
-            for i in range(top, bottom + 1):
-                ans.append(matrix[i][right])
-            right -= 1
-
-            # Traverse from right to left, if still within bounds
-            if top <= bottom:
-                for i in range(right, left - 1, -1):
-                    ans.append(matrix[bottom][i])
-                bottom -= 1
-
-            # Traverse from bottom to top, if still within bounds
-            if left <= right:
-                for i in range(bottom, top - 1, -1):
-                    ans.append(matrix[i][left])
-                left += 1
-
-        return ans
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    res.append(row.pop(0))
+        return res
