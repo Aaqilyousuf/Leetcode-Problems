a8bbe4bee93e84b73eb7a6d9d21087363d52b89f
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-01-18 17:37:24 +0530

    Time: 3 ms (72.86%), Space: 17.8 MB (45.78%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,38 +5,6 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        #worst brute force ever know to mankind
-    #     if not l1 and not l2:
-    #         return 
-    #     if not l1:
-    #         return l2
-    #     if not l2:
-    #         return l1
-    #     cur1 = l1
-    #     cur2 = l2
-    #     list1 = []
-    #     list2 = []
-    #     while cur1:
-    #         list1.append(cur1.val)
-    #         cur1 = cur1.next
-    #     while cur2:
-    #         list2.append(cur2.val)
-    #         cur2 = cur2.next
-    #     num1 = int(''.join(map(str, list1[::-1])))
-    #     num2 = int(''.join(map(str, list2[::-1])))
-    #     result = num1 + num2        
-    #     resL = list(map(int, str(result)[::-1]))
-    #     finalRes = self.insert(resL)
-    #     return finalRes
-    # def insert(self, resList):
-    #     head = ListNode(resList[0])
-    #     cur = head
-    #     for i in range(1, len(resList)):
-    #         newNode = ListNode(resList[i])
-    #         cur.next = newNode
-    #         cur = newNode
-    #     return head
-    #OPTIMAL SOLUTION
         dummy = ListNode()
         temp = dummy
         carry = 0
@@ -54,7 +22,3 @@ class Solution:
             temp.next = node
             temp = temp.next
         return dummy.next
-
-
-
-
