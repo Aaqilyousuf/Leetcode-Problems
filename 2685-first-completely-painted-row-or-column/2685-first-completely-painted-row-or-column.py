9c5671d6015a19a5d902fd5ade2f92fb97d9c9a2
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-01-20 15:32:18 +0530

    Time: 131 ms (49.21%), Space: 53.4 MB (25.24%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.py
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.py
@@ -2,14 +2,14 @@ class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
         n = len(mat)
         m = len(mat[0])
-        hashMap = defaultdict(list)
+        hashMap = defaultdict(list) #it could be normal dict (hashMap = {})
 
         for r in range(n):
             for c in range(m):
                 hashMap[mat[r][c]] = [r, c]
         rowFreq = [0] * n
         colFreq = [0] * m
-        for i in range(m*n):
+        for i in range(n*m):
             if arr[i] in hashMap:
                 r, c = hashMap[arr[i]]
                 rowFreq[r] += 1
