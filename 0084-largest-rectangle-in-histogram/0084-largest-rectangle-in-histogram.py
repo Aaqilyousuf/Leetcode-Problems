6a421f5b092da6c523a4226a8ecd9c64e814bbff
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-01-22 09:10:04 +0530

    Time: 151 ms (75.38%), Space: 29.5 MB (76.87%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,14 +1,33 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        # maxArea = 0
+        # stack = []
+        # for i, h in enumerate(heights):
+        #     start = i
+        #     while stack and stack[-1][1] > h:
+        #         index, height = stack.pop()
+        #         start = index
+        #         maxArea = max(maxArea, height * (i-index))            
+        #     stack.append((start, h))
+        # for i, h in stack:
+        #     maxArea = max(maxArea, h * (len(heights) - i))
+        # return maxArea
         maxArea = 0
-        stack = []
-        for i, h in enumerate(heights):
-            start = i
-            while stack and stack[-1][1] > h:
-                index, height = stack.pop()
-                start = index
-                maxArea = max(maxArea, height * (i-index))            
-            stack.append((start, h))
-        for i, h in stack:
-            maxArea = max(maxArea, h * (len(heights) - i))
+        n = len(heights)
+        stack = [] #stores only the index
+        for i in range(len(heights)):
+            while stack and heights[stack[-1]] > heights[i]:
+                ind = stack.pop()
+                nse = i #next smallest element
+                pse = -1 if not stack else stack[-1] #previous smallest emelent
+                area = heights[ind] * (nse-pse-1)
+                maxArea = max(maxArea, area)
+            stack.append(i)
+        while stack:
+            ind = stack.pop()
+            nse = n
+            pse = -1 if not stack else stack[-1]
+            area = heights[ind] * (nse-pse-1)
+            maxArea = max(maxArea, area)
         return maxArea
+
