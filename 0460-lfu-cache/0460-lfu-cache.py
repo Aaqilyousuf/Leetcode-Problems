e8a2d1ea78b93222e6e3cd8d92b61adaf9114c61
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-01-29 18:47:24 +0530

    Time: 166 ms (42.19%), Space: 79.6 MB (73.14%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.py
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -2,20 +2,20 @@ class Node:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.cnt = 1  # Frequency count
+        self.cnt = 1  
         self.prev = None
         self.next = None
 
 class DoublyLinkedList:
     def __init__(self):
-        self.head = Node(0, 0)  # Dummy head
-        self.tail = Node(0, 0)  # Dummy tail
+        self.head = Node(0, 0)  
+        self.tail = Node(0, 0) 
         self.head.next = self.tail
         self.tail.prev = self.head
         self.size = 0
 
     def add_front(self, node):
-        """Add a node right after head (front of the list)."""
+        #add front to the dll
         temp = self.head.next
         node.next = temp
         node.prev = self.head
@@ -24,7 +24,7 @@ class DoublyLinkedList:
         self.size += 1
 
     def remove_node(self, node):
-        """Remove a node from the list."""
+    
         prev_node = node.prev
         next_node = node.next
         prev_node.next = next_node
@@ -36,20 +36,20 @@ class LFUCache:
         self.max_size = capacity
         self.min_freq = 0
         self.cur_size = 0
-        self.key_node = {}  # Maps key -> Node
-        self.freq_list_map = {}  # Maps frequency -> DoublyLinkedList
+        self.key_node = {}  #maps key -> Node
+        self.freq_list_map = {}  #maps frequency -> DoublyLinkedList
 
     def update_freq_list(self, node):
-        """Update the frequency of a node and move it to the next frequency list."""
+
         key = node.key
         self.key_node.pop(key)
 
-        # Remove node from current frequency list
+        #remove node from current frequency list
         self.freq_list_map[node.cnt].remove_node(node)
         if node.cnt == self.min_freq and self.freq_list_map[node.cnt].size == 0:
-            self.min_freq += 1  # If no nodes left in min_freq, increment
+            self.min_freq += 1  #if no nodes left in min_freq, increment
 
-        # Move node to next higher frequency list
+        #move node to next higher frequency list
         node.cnt += 1
         if node.cnt not in self.freq_list_map:
             self.freq_list_map[node.cnt] = DoublyLinkedList()
@@ -57,7 +57,7 @@ class LFUCache:
         self.key_node[key] = node
 
     def get(self, key: int) -> int:
-        """Retrieve a value from the cache and update its frequency."""
+        
         if key in self.key_node:
             node = self.key_node[key]
             self.update_freq_list(node)
@@ -65,9 +65,9 @@ class LFUCache:
         return -1
 
     def put(self, key: int, value: int):
-        """Insert or update a key-value pair in the LFU cache."""
+
         if self.max_size == 0:
-            return  # If capacity is 0, no caching is possible
+            return  #if capacity is 0, no caching is possible
 
         if key in self.key_node:
             node = self.key_node[key]
@@ -75,14 +75,14 @@ class LFUCache:
             self.update_freq_list(node)
         else:
             if self.cur_size == self.max_size:
-                # Evict the least frequently used node
+                #evict the least frequently used node
                 min_freq_list = self.freq_list_map[self.min_freq]
-                evict_node = min_freq_list.tail.prev  # Least recently used node in min frequency list
+                evict_node = min_freq_list.tail.prev  #least recently used node in min frequency list
                 self.key_node.pop(evict_node.key)
                 min_freq_list.remove_node(evict_node)
                 self.cur_size -= 1
 
-            # Insert new node
+            #insert new node
             self.cur_size += 1
             self.min_freq = 1
             new_node = Node(key, value)
