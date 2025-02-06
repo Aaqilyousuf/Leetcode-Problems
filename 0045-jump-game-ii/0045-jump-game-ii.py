4ca208461485f7dd27dc6fb159d25c2305536eb0
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-06 15:01:49 +0530

    Time: 3 ms (97.33%), Space: 18.7 MB (49.35%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+       
+        count = 0
+        jump = 0
+        curInd = 0
+        for i in range(len(nums)-1):
+            jump = max(jump, nums[i]+i)
+
+            if i==curInd:
+                count += 1
+                curInd = jump
+        return count
+        
