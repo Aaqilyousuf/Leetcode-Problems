6c371e4605f9139d7ba7ed67ee843a35408fc5a8
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-08 08:58:14 +0530

    Time: 0 ms (100%), Space: 18 MB (49.76%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,12 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # per = permutations(nums)
-        # res1 = list(per)
-        # return res1
         n = len(nums)
         ans, sol = [], []
 
         def backtrack():
             if len(sol) == n:
                 ans.append(sol[:])
-                return 
+                return
             for x in nums:
                 if x not in sol:
                     sol.append(x)
