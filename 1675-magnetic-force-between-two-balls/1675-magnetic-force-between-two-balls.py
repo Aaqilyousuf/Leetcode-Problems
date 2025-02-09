4b9a1b05b20a41a895878e34b80dfa3593f71933
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-09 17:03:03 +0530

    Time: 491 ms (54.44%), Space: 30.2 MB (84.76%) - LeetHub

--- 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.py
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.py
@@ -2,25 +2,23 @@ class Solution:
     def maxDistance(self, position: List[int], m: int) -> int:
         n = len(position)
         position.sort()
-        low = 1
-        high = position[n-1] - position[0]
-        ans = -1
-        while low<=high:
-            mid = low + (high-low)//2
-            if self.canWePlace(position, mid, m):
-                ans = mid
-                low = mid + 1
+        l, r = 1, position[-1] - position[0]
+        best = -1
+        while l<=r:
+            mid = (l+r)//2
+            if self.helper(position, mid, m):
+                best = mid
+                l = mid+1
             else:
-                high = mid - 1
-        return ans
-    def canWePlace(self, pos, mid, m):
-        cntBall = 1
-        sum = pos[0]
-        for i in range(1, len(pos)):
-            if pos[i] - sum >= mid:
-                cntBall += 1
-                sum = pos[i]
-            if cntBall >= m:
-                return True
+                r = mid-1
+        return best
+    def helper(self, nums, maxT, m):
+        balls = 1
+        sumPos = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] - sumPos >= maxT:
+                balls += 1
+                sumPos = nums[i]
+                if balls == m:
+                    return True
         return False
-
