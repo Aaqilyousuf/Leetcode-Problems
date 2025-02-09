aacb91fa71ce14cd0c7cc39e5fa3583ff49b2922
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-09 16:14:25 +0530

    Time: 4 ms (33.81%), Space: 18.1 MB (23.42%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,29 +1,23 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        low = min(nums)
-        high = sum(nums)
-        res = -1
-        while low<=high:
-            mid = low + (high-low)//2
-
-            if self.findPossible(nums, mid, k):
-                res = mid
-                high = mid - 1
+        def helper(nums, maxT):
+            split = 1
+            sum = 0
+            for n in nums:
+                if n+sum > maxT:
+                    split += 1
+                    sum = n
+                    if split > k:
+                        return False
+                else:
+                    sum += n
+            return True
+        l = max(nums)
+        r = sum(nums)
+        while l<=r:
+            m = l+(r-l)//2
+            if helper(nums, m):
+                r = m-1
             else:
-                low = mid + 1
-        return res
-    def findPossible(self, nums, mid, k):
-        s = 1
-        e = 0
-
-        for i in range(len(nums)):
-            if nums[i] > mid:
-                return False
-            if e + nums[i] > mid:
-                s += 1
-                e = nums[i]
-            else:
-                e += nums[i]
-        if s > k:
-            return False
-        return True
+                l = m+1
+        return l
