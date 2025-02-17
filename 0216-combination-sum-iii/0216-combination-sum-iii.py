6fa4f99488a0aadd1c4496d90d09a8483957ef85
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-17 08:42:47 +0530

    Time: 0 ms (100%), Space: 17.6 MB (86.53%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,16 +1,8 @@
 class Solution:
     def combinationSum3(self, k: int, n: int, start = 1) -> List[List[int]]:
-        if k == 1:
-            if start <= n <=9:
-                return [[n]]
-            else:
-                return []
-        res = []
-        for i in range(start,9):
-            tmp = self.combinationSum3(k-1,n-i,i+1)
-            for j in tmp:
-                j.insert(0,i)
-                res.append(j)
+        nums = [i for i in range(1, 10)]
+        combo = itertools.combinations(nums, k)
+        res = [i for i in combo if sum(i)==n]
         return res
 
             
