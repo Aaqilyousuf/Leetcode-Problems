f33f1b9ee03d83d736794685f18979bfaf39a11c
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-17 08:46:43 +0530

    Time: 0 ms (100%), Space: 18 MB (17.34%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,8 +1,20 @@
 class Solution:
     def combinationSum3(self, k: int, n: int, start = 1) -> List[List[int]]:
-        nums = [i for i in range(1, 10)]
-        combo = itertools.combinations(nums, k)
-        res = [i for i in combo if sum(i)==n]
+        # nums = [i for i in range(1, 10)]
+        # combo = itertools.combinations(nums, k)
+        # res = [i for i in combo if sum(i)==n]
+        # return res
+        res = []
+        def dfs(start, nums, tot):
+            if len(nums)==k:
+                if tot==n:
+                    res.append(nums[:])
+                return
+            for i in range(start, 10):
+                if tot+i>n:
+                    break
+                dfs(i+1, nums+[i], tot+i)
+        dfs(1, [], 0)
         return res
 
             
