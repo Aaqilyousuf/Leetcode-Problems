e62ae106a93d03a3d24976c2f5e6c83f1f575c20
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-24 19:58:46 +0530

    Time: 70 ms (14.11%), Space: 27.5 MB (6.45%) - LeetHub

---
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        row, col = len(matrix), len(matrix[0])
+        memo = {}
+        def backtrack(x, y):
+            if y<0 or y>=col:
+                return float('inf')
+            if (x,y) in memo:
+                return memo[(x,y)]
+            if x==row-1:
+                return matrix[x][y]
+            minSum = matrix[x][y] + min(backtrack(x+1,y-1), backtrack(x+1,y), backtrack(x+1,y+1))
+            memo[(x,y)] = minSum
+            return minSum
+        return min(backtrack(0, y) for y in range(col))
+            
