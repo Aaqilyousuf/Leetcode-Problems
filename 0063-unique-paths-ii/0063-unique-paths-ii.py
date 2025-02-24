0ee8dbeafa57a2cd81245a200bf5710b615a81d9
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-24 14:26:19 +0530

    Time: 5 ms (3.31%), Space: 18 MB (31%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        row, col = len(obstacleGrid), len(obstacleGrid[0])
+        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1]:
+            return 0
+        memo = {}
+        def backtrack(x, y):
+            if x==row-1 and y==col-1:
+                return 1
+            if (x,y) in memo:
+                return memo[(x,y)]
+            if x>=row or y>=col or obstacleGrid[x][y]==1:
+                return 0
+            count = 0
+            count += (backtrack(x+1, y) + backtrack(x, y+1))
+            memo[(x,y)] = count
+            return memo[(x,y)]
+        return backtrack(0, 0)
+            
+            
