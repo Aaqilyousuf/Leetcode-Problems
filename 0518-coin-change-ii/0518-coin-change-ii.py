cfd13adaa7f72f3c7301934ecc41574f702b4282
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-27 18:18:12 +0530

    Time: 119 ms (91.1%), Space: 17.8 MB (98.73%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,16 +1,25 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        memo = {}
-        def unboundedKnapsack(i, target):
-            if target == 0:
-                return 1
-            if target<0 or i>=len(coins):
-                return 0
-            if (i, target) in memo:
-                return memo[(i, target)]
-            inc = unboundedKnapsack(i, target-coins[i])
-            exc = unboundedKnapsack(i+1, target)
-            memo[(i, target)] = inc + exc
-            return memo[(i, target)]
+        # memo = {}
+        # def unboundedKnapsack(i, target):
+        #     if target == 0:
+        #         return 1
+        #     if target<0 or i>=len(coins):
+        #         return 0
+        #     if (i, target) in memo:
+        #         return memo[(i, target)]
+        #     inc = unboundedKnapsack(i, target-coins[i])
+        #     exc = unboundedKnapsack(i+1, target)
+        #     memo[(i, target)] = inc + exc
+        #     return memo[(i, target)]
 
-        return unboundedKnapsack(0, amount)
+        # return unboundedKnapsack(0, amount)
+
+        dp = [0] * (amount + 1)
+        dp[0] = 1 
+
+        for coin in coins:
+            for current_amount in range(coin, amount + 1):
+                dp[current_amount] += dp[current_amount - coin]
+
+        return dp[amount]
