434b64fcc09d705d524ab3a9801d3e7864fdd70b
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-27 12:12:37 +0530

    Time: 1543 ms (6.13%), Space: 45.7 MB (8.99%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -10,9 +10,29 @@ class Solution:
         #         amount -= coins[i]
         #         ans += 1
         # return ans
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
-        for coin in coins:
-            for i in range(coin, amount+1):
-                dp[i] = min(dp[i], dp[i-coin]+1)
-        return dp[amount] if dp[amount] != float("inf") else -1
+        #DP Table
+        # dp = [float('inf')] * (amount + 1)
+        # dp[0] = 0
+        # for coin in coins:
+        #     for i in range(coin, amount+1):
+        #         dp[i] = min(dp[i], dp[i-coin]+1)
+        # return dp[amount] if dp[amount] != float("inf") else -1
+        memo = {}
+        def minCoin(rem):
+            if rem==0:
+                return 0
+            if rem<0:
+                return float('inf')
+            if rem in memo:
+                return memo[rem]
+            mini = float('inf')
+            for coin in coins:
+                res = minCoin(rem-coin)
+                if res != float('inf'):
+                    mini = min(mini, res+1)
+                
+            memo[rem] = mini
+            return mini
+        res = minCoin(amount)
+        return res if res!=float('inf') else -1
+        
