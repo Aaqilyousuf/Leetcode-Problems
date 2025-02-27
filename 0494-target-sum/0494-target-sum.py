babe1a58269b1cfc71cfdd437497d55b4e4c98f5
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-02-27 17:57:26 +0530

    Time: 199 ms (35.83%), Space: 38.5 MB (18.41%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,19 +1,28 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        n=len(nums)
-        Sum=sum(nums)
-        diff=Sum-target
+        # n=len(nums)
+        # Sum=sum(nums)
+        # diff=Sum-target
 
-        if diff<0 or diff%2==1: return 0
-        diff/=2
+        # if diff<0 or diff%2==1: return 0
+        # diff/=2
         
-        @cache
-        def f(j , sum):
-            if j==0: return 1 if sum==0 else 0
-            x=nums[j-1]
-            ans=f(j-1, sum)
-            if sum>=x:
-                ans+=f(j-1, sum-x)
-            return ans
-        return f(n, diff)
+        # @cache
+        # def f(j , sum):
+        #     if j==0: return 1 if sum==0 else 0
+        #     x=nums[j-1]
+        #     ans=f(j-1, sum)
+        #     if sum>=x:
+        #         ans+=f(j-1, sum-x)
+        #     return ans
+        # return f(n, diff)
+        memo = {}
+        def backtrack(i, tot):
+            if i==len(nums):
+                return 1 if tot == target else 0
+            if (i, tot) in memo:
+                return memo[(i, tot)]
+            memo[(i, tot)] = (backtrack(i+1, tot+nums[i]) + backtrack(i+1, tot-nums[i]))
+            return memo[(i, tot)]
+        return backtrack(0, 0)
         
