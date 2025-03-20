719f07805be5055f406e23768ace4f70c1107bab
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-03-20 14:51:39 +0530

    Time: 7 ms (96.43%), Space: 18 MB (34.87%) - LeetHub

--- 1022-unique-paths-iii/1022-unique-paths-iii.py
+++ 1022-unique-paths-iii/1022-unique-paths-iii.py
@@ -1,7 +1,7 @@
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
         zero, sx, sy = 0, 0, 0
-        # Count zeros and find the starting point
+       
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col] == 1:
@@ -10,25 +10,24 @@ class Solution:
                     zero += 1
 
         def dfs(grid, sx, sy, zero):
-            # Out of bounds or hits an obstacle
+           
             if sx < 0 or sy < 0 or sx >= len(grid) or sy >= len(grid[0]) or grid[sx][sy] == -1:
                 return 0
             
-            # Reached the destination (2), and we must have walked over all 0s (i.e., zero == -1)
+            
             if grid[sx][sy] == 2:
                 return 1 if zero == -1 else 0
             
-            # Mark the cell as visited
+        
             grid[sx][sy] = -1
             zero -= 1
             
-            # Explore all 4 directions
+           
             totalPath = (dfs(grid, sx + 1, sy, zero) + 
                          dfs(grid, sx, sy + 1, zero) + 
                          dfs(grid, sx - 1, sy, zero) + 
                          dfs(grid, sx, sy - 1, zero))
             
-            # Unmark the cell (backtracking)
             grid[sx][sy] = 0
             zero += 1
 
