1cad824e267a57240625f3a2bb47240112554ea8
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-03-22 12:35:08 +0530

    Time: 4 ms (69.89%), Space: 18.8 MB (93.22%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,30 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #topological sort method
-        graph = defaultdict(list)
-        for c, pre in prerequisites:
-            graph[pre].append(c)
-        visited = set()
-        # stack = []
-        cycle = set()
-        
-        def dfs(node):
-            if node in cycle:
-                return False
-            if node in visited:
-                return True
-            cycle.add(node)
-            for neighbour in graph[node]:
-                if not dfs(neighbour):
-                    return False
-            cycle.remove(node)
-            visited.add(node)
-            # stack.append(node)
-            return True
+        #topological sort method (khans algo
+        adj = {}
+        for i in range(numCourses):
+            adj[i] = []
+        indegree = [0] * numCourses #num of edges connected to that current node
+        for course, pre in prerequisites:
+            adj[pre].append(course)
+            indegree[course] += 1
+        queue = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        count = 0
+        while queue:
+            node = queue.popleft()
+            count += 1
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    queue.append(nei)
+        return count == numCourses
 
-
-        for node in range(numCourses):
-            if node not in visited:
-                if not dfs(node):
-                    return False
-        return True
