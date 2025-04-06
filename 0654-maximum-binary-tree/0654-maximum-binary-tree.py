42ea44c8f2b8322a3f5318d093589593e9d07639
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-04-06 08:03:58 +0530

    Time: 15 ms (93.68%), Space: 18.2 MB (55.02%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.py
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.py
@@ -6,15 +6,29 @@
 #         self.right = right
 class Solution:
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
-        mx = max(nums)
-        maxInd = nums.index(mx)
-        node = TreeNode(mx)
-        if maxInd > 0:
-            node.left = self.constructMaximumBinaryTree(nums[:maxInd])
-        if maxInd < len(nums)-1:
-           node.right = self.constructMaximumBinaryTree(nums[maxInd+1:])
+        # #recursive approach divide and conq
+        # mx = max(nums)
+        # maxInd = nums.index(mx)
+        # node = TreeNode(mx)
+        # if maxInd > 0:
+        #     node.left = self.constructMaximumBinaryTree(nums[:maxInd])
+        # if maxInd < len(nums)-1:
+        #    node.right = self.constructMaximumBinaryTree(nums[maxInd+1:])
         
-        return node
-        
+        # return node
+
+        #stack approach
+
+        nodes = []
+        for num in nums:
+            node = TreeNode(num)
+            while nodes and nodes[-1].val<num:
+                node.left = nodes.pop()
+            if nodes:
+                nodes[-1].right = node
+            
+            nodes.append(node)
+
+        return nodes[0]
             
         
