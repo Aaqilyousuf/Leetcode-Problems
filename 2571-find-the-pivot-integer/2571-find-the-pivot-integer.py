377f36ef4cbf4333d57f56b14ec188329f9b6fd0
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-04-08 07:16:15 +0530

    Time: 15 ms (43.58%), Space: 17.7 MB (85.6%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
@@ -2,8 +2,8 @@ class Solution:
     def pivotInteger(self, n: int) -> int:
         tot = n*(n+1)//2
         for x in range(1, n+1):
-            uptoX = x*(x+1)//2
-            fromX = tot - (x-1)*x//2
-            if uptoX == fromX:
+            uptoX = x*(x+1)//2 #sum from 1 to x
+            fromX = tot - (x-1)*x//2 # sum from x to n
+            if uptoX == fromX: #if both matches then x is pivot integer
                 return x
         return -1
