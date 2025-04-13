eb140fb754a00b9de0fb8b7dd71d7ab63cea37cc
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-04-13 20:24:12 +0530

    Time: 4 ms (65.7%), Space: 19.8 MB (74.26%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -6,20 +6,33 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        val = []
-        stack = []
-        while stack or root:
-            while root:
-                stack.append(root)
+        # val = []
+        # stack = []
+        # while stack or root:
+        #     while root:
+        #         stack.append(root)
                 
-                root = root.left
-            root = stack.pop()
-            val.append(root.val)
-            root = root.right
-        hashMap = {}
-        for i, num in enumerate(val):
-            comp = k - num
-            if comp in hashMap:
+        #         root = root.left
+        #     root = stack.pop()
+        #     val.append(root.val)
+        #     root = root.right
+        # hashMap = {}
+        # for i, num in enumerate(val):
+        #     comp = k - num
+        #     if comp in hashMap:
+        #         return True
+        #     hashMap[num] = i
+        # return False
+        seen = set()
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            if (k-node.val in seen):
                 return True
-            hashMap[num] = i
+            else:
+                seen.add(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
         return False
