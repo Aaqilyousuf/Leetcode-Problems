e225b172e188690989557fc5e19e5f89d32438ff
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-05-04 19:33:46 +0530

    Time: 3 ms (81.48%), Space: 18.8 MB (79.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -8,10 +8,17 @@ class Solution:
         #     if nums[l] == 0:
         #         nums[l], nums[r] = nums[r], nums[l]
         #         l += 1
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] == 0:
-                    nums[i], nums[j] = nums[j], nums[i]
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] == 0:
+        #             nums[i], nums[j] = nums[j], nums[i]
 
+        l = 0  
+        for r in range(len(nums)):  
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]  
+                l += 1  
+       
+   
 
         
