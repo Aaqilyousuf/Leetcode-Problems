0a25d61b30aabd4dcd6e1e06eed9e6369964a5c6
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-05-19 06:50:02 +0530

    Time: 0 ms (100%), Space: 17.8 MB (31.6%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
@@ -2,11 +2,11 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         a = 0
         res = 0
-        for i in s:
-            if i=="(":
-                a+=1
+        for i in range(len(s)):
+            if s[i] == '(':
+                a += 1
             elif a>0:
-                a-=1
+                a -= 1
             else:
-                res+=1
-        return res+a        
+                res += 1
+        return res+a
