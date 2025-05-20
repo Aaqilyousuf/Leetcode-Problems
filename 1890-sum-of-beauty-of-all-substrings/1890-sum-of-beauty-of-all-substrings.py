aba754c49d715a1d43ffddcf47d368b8178bac5e
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-05-20 08:20:59 +0530

    Time: 1158 ms (94.39%), Space: 17.8 MB (56.92%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.py
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.py
@@ -1,19 +1,11 @@
 class Solution:
     def beautySum(self, s: str) -> int:
+        n = len(s)
         res = 0
-        for i in range(len(s)):
-            alpha = [0] * 26  # Frequency array for characters 'a' to 'z'
-            for j in range(i, len(s)):
-                alpha[ord(s[j]) - ord('a')] += 1
-                res += self.beauty(alpha)
+        for i in range(n):
+            freq = defaultdict(int)
+            for j in s[i:]:
+                freq[j] += 1
+                vals = freq.values()
+                res += max(vals) - min(vals)
         return res
-
-    def beauty(self, alpha):
-        max_freq = max(alpha)  # Maximum frequency of any character
-        min_freq = float('inf')
-
-        for freq in alpha:
-            if freq >= 1:
-                min_freq = min(min_freq, freq)
-
-        return max_freq - min_freq
