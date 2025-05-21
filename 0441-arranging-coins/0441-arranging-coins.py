9c4f1340d7e1bf31afcf19605c32b4c838b85413
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-05-21 10:26:15 +0530

    Time: 407 ms (31.7%), Space: 17.9 MB (33.2%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,14 +1,19 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        low, high = 0, n
+        # low, high = 0, n
 
-        while low<=high:
-            mid = (low+high) // 2
-            conis_needed = mid*(mid+1) // 2
-            if conis_needed == n:
-                return mid
-            if conis_needed < n:
-                low = mid + 1
-            else:
-                high = mid - 1
-        return high
+        # while low<=high:
+        #     mid = (low+high) // 2
+        #     conis_needed = mid*(mid+1) // 2
+        #     if conis_needed == n:
+        #         return mid
+        #     if conis_needed < n:
+        #         low = mid + 1
+        #     else:
+        #         high = mid - 1
+        # return high
+        i = 1
+        while i<=n:
+            n -= i
+            i += 1
+        return i-1
