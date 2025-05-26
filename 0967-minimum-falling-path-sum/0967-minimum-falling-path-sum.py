0195d5c173a100350e53b4e133031140f64d5ddb
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-05-26 13:00:06 +0530

    Time: 75 ms (10.07%), Space: 27.6 MB (6.76%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -12,5 +12,12 @@ class Solution:
             minSum = matrix[x][y] + min(backtrack(x+1,y-1), backtrack(x+1,y), backtrack(x+1,y+1))
             memo[(x,y)] = minSum
             return minSum
-        return min(backtrack(0, y) for y in range(col))
+
+        min_path_sum = float('inf')  
+        for y in range(col):
+            path_sum = backtrack(0, y)
+            if path_sum < min_path_sum:
+                min_path_sum = path_sum
+        return min_path_sum
+        # return min(backtrack(0, y) for y in range(col))
             
