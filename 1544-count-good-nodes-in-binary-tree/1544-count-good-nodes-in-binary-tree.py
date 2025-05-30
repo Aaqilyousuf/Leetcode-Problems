23833470c07dc2c64a8cbd8c71fd6c3875dead54
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-05-30 10:28:39 +0530

    Time: 134 ms (79.18%), Space: 32.3 MB (33.09%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -10,13 +10,13 @@ class Solution:
         def dfs(node, max_val):
             if not node:
                 return 0
-            # Check if the current node is a good node
+        
             good_node_count = 1 if node.val >= max_val else 0
-            # Update the maximum value seen so far
+           
             max_val = max(max_val, node.val)
-            # Recursively count good nodes in the left and right subtrees
+           
             good_node_count += dfs(node.left, max_val)
             good_node_count += dfs(node.right, max_val)
             return good_node_count
         
-        return dfs(root, root.val)  # Start with the root value as the initial max
+        return dfs(root, root.val)
