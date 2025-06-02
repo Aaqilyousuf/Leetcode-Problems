9c911a9fc7f6a538e0cf7fdb9d52d25bbb547978
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-06-02 18:52:00 +0530

    Time: 15 ms (69.14%), Space: 20 MB (17.61%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,14 +1,14 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
-        candies = [1] * n  # Give each child at least one candy
+        candies = [1] * n  #each child at least one candy
 
-    # Left to right pass
+        #Left to right pass
         for i in range(1, n):
             if ratings[i] > ratings[i - 1]:
                 candies[i] = candies[i - 1] + 1
 
-    # Right to left pass
+        #right to left pass
         for i in range(n - 2, -1, -1):
             if ratings[i] > ratings[i + 1]:
                 candies[i] = max(candies[i], candies[i + 1] + 1)
