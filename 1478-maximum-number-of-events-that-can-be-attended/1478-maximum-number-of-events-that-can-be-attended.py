876cd285e4ae1d3fece1c7fcad0c924bb7f9a4b1
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-07-07 12:01:20 +0530

    Time: 142 ms (64.74%), Space: 53.2 MB (84.59%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
         events.sort()
-        minHeap = []
+        minHeap = []#to store the end time
         day = 0
         attended = 0
         i = 0
@@ -12,9 +12,9 @@ class Solution:
             while i < n and events[i][0] <= day:
                 heapq.heappush(minHeap, events[i][1])#endDay
                 i += 1
-            while minHeap and minHeap[0] < day:
+            while minHeap and minHeap[0] < day:#3xpired events
                 heapq.heappop(minHeap)
-            if minHeap:
+            if minHeap:#if not then attend and pop the end time
                 heapq.heappop(minHeap)
                 attended += 1
             day += 1
