11dc7640f4e2b926d96550f7f85ff0c9807d7595
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-07-15 12:42:42 +0530

    Time: 467 ms (85.77%), Space: 19.1 MB (25.68%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -6,9 +6,24 @@ class Solution:
             ps.append(i*i)
             i += 1
         print(ps)
-        dp = [float('inf')] * (n+1)
-        dp[0] = 0
-        for p in ps:
-            for i in range(p, n+1):
-                dp[i] = min(dp[i], dp[i-p] + 1)
-        return dp[n] if dp[n] != float('inf') else -1
+        #DP bottom up table
+        # dp = [float('inf')] * (n+1)
+        # dp[0] = 0
+        # for p in ps:
+        #     for i in range(p, n+1):
+        #         dp[i] = min(dp[i], dp[i-p] + 1)
+        # return dp[n] if dp[n] != float('inf') else -1
+        #Using BFS
+        queue = deque([(n, 0)]) #n, steps
+        visited = set()
+        while queue:
+            rem, steps = queue.popleft()
+            if rem == 0:
+                return steps
+            for sq in ps:
+                newRem = rem-sq
+                if newRem < 0:
+                    break
+                if newRem not in visited:
+                    visited.add(newRem)
+                    queue.append((newRem, steps+1))
