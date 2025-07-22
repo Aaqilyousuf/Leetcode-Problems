b5b2f53d4b0ac53f2b800d666ff0f45efe81d0da
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-07-22 12:18:31 +0530

    Time: 27 ms (59.3%), Space: 18.5 MB (82.33%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -8,7 +8,7 @@ class Solution:
         for i in range(len(nums)):
             if i>jump:
                 return False
-            jump = max(jump, nums[i]+i) #current element + index
+            jump = max(jump, nums[i]+i) #current element + index gives the farthest index we can reach from i
             if jump >= len(nums)-1:
                 return True
             
