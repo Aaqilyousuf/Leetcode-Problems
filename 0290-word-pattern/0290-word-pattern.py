7b7bc628d3f74d206158222cc95c4de92e63a66b
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-07-22 11:44:49 +0530

    Time: 0 ms (100%), Space: 17.9 MB (34.94%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,11 +1,31 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        ls = s.split()
-        patternInd = []
-        for p in pattern:
-            patternInd.append(pattern.index(p))
-        sind = []
-        for word in ls:
-            sind.append(ls.index(word))
+        #n2
+        # ls = s.split()
+        # patternInd = []
+        # for p in pattern:
+        #     patternInd.append(pattern.index(p))
+        # sind = []
+        # for word in ls:
+        #     sind.append(ls.index(word))
         
-        return patternInd == sind
+        # return patternInd == sind
+
+        ls = s.split()
+        if len(ls) != len(pattern):
+            return False
+        ptos = {}
+        stop = {}
+        for p, w in zip(pattern, ls):
+            if p in ptos:
+                if ptos[p] != w:
+                    return False
+            else:
+                ptos[p] = w
+            
+            if w in stop:
+                if stop[w] != p:
+                    return False
+            else:
+                stop[w] = p
+        return True
