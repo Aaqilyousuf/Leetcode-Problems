fd8b18ace20abccf8084beb26096ac4ceb8d57ad
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-07-25 06:50:55 +0530

    Time: 0 ms (100%), Space: 17.5 MB (98.13%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
+        #worst time O(n2)
         if len(nums) == 1:
             return nums[0]
         stack = []
@@ -7,8 +8,6 @@ class Solution:
         for i in range(len(nums)):
             if stack and nums[i] in stack:
                 stack.remove(nums[i])
-
-                
             if nums[i] < 0:
                 continue
             stack.append(nums[i])
@@ -18,3 +17,22 @@ class Solution:
             return max(stack)
         print(stack)
         return sum(stack)
+        #this fails
+        # seen = set()
+        # l = 0
+        # curSum = 0
+        # maxSum = nums[0]
+        # for r in range(len(nums)):
+        #     while nums[r] in seen:
+        #         seen.remove(nums[l])
+        #         curSum -= nums[l]
+        #         l += 1
+
+        #     seen.add(nums[r])
+        #     curSum += nums[r]
+        #     maxSum = max(maxSum, curSum)
+        #     if curSum < 0:
+        #         seen.clear()
+        #         curSum = 0
+        #         l = r + 1
+        # return maxSum
