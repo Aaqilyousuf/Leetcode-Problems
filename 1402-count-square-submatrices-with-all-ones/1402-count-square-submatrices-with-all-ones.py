618cdb0fe2077e4aaecaf247420ae8e6224cfafc
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-08-20 06:26:47 +0530

    Time: 59 ms (58.41%), Space: 20.4 MB (52.6%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -3,13 +3,13 @@ class Solution:
         row, col = len(matrix), len(matrix[0])
         dp = [[0]*col for _ in range(row)]
 
-        for j in range(col):
+        for j in range(col):#Each column
             dp[0][j] = matrix[0][j]
 
-        for i in range(row):
+        for i in range(row):#Each row
             dp[i][0] = matrix[i][0]
 
-        for i in range(1, row):
+        for i in range(1, row):#starting form (1,1)
             for j in range(1, col):
                 if matrix[i][j] == 1:
                     dp[i][j] = 1+min(dp[i-1][j],dp[i-1][j-1],dp[i][j-1])
