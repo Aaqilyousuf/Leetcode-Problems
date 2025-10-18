155220fc70ad9f35c22b5f4483e1e0195cf8a0f7
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-10-18 20:19:52 +0530

    Time: 12 ms (13.49%), Space: 19.8 MB (21.9%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        xor = 0
-        for i in range(len(nums)):
-            xor = xor ^ nums[i]
-        return xor
+        # xor = 0
+        # for i in range(len(nums)):
+        #     xor = xor ^ nums[i]
+        # return xor
+        if len(nums) == 1:
+            return nums[0]
+        mp = defaultdict(int)
+        for n in nums:
+            mp[n] += 1
+        for n in mp:
+            if mp[n] == 1:
+                return n
+        return -1
