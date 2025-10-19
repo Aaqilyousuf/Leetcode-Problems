a8740f1c96c7dc680484d38701be90febd92222e
Aaqil Yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2025-10-19 08:40:47 +0530

    Time: 3 ms (4.86%), Space: 17.8 MB (41.29%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,8 +3,7 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        l = 0
-        r = len(matrix) - 1
+        l, r = 0, len(matrix)-1
         while l < r:
             matrix[l], matrix[r] = matrix[r], matrix[l]
             l += 1
@@ -12,4 +11,5 @@ class Solution:
         for i in range(len(matrix)):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
                 
